4f184896359ae086aba46a9030b4f048293656dd
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-08-18 13:13:34 +1000

    Toggle switch for light/dark mode: added CSS; got basic functionality to switch between modes, including addition of mystery modes

--- js/modes.js
+++ js/modes.js
@@ -1,68 +1,64 @@
-const lightbutton = document.getElementById("light");
-const darkbutton = document.getElementById("dark");
-const mysterybutton = document.getElementById("mystery");
-const mineralbutton = document.getElementById("mineral");
-const neonbutton = document.getElementById("neon");
-const ysbutton = document.getElementById("yellowsub")
-
-const mysteryModes = [
-    // "mineral",
-    // "neon",
-    "yellowsub",
-];
+const modes = document.getElementById("modes");
 const stylesheets = document.getElementsByClassName("modesheet");
+const lightbutton = document.getElementById("lightmode");
+const darkbutton = document.getElementById("darkmode");
+const mysterybutton = document.getElementById("mysterymode");
+let activemode = "darkmode";
+let isMysteryActive = false;
+const mysteryModes = [
+    "redmode",
+    "greenmode",
+    "bluemode",
+];
 
-lightbutton.onclick = function() {
-    SwitchMode("lightmode");
-};
-darkbutton.onclick = function() {
-    if (mysteryModes.length > 0) {
-        mysterybutton.style.display = "unset";
-    }; 
-    SwitchMode("darkmode");
-};
-mysterybutton.onclick = function() {
+darkbutton.addEventListener("click", () => {SwitchMode("darkmode");});
+lightbutton.addEventListener("click", () => {SwitchMode("lightmode");});
+mysterybutton.addEventListener("click", () => {
     let mode = mysteryModes[Math.floor(Math.random() * mysteryModes.length)];
     const index = mysteryModes.indexOf(mode);
     mysteryModes.splice(index, 1);
-    console.log(mysteryModes);
     if (mysteryModes.length < 1) {
         mysterybutton.style.display = "none";
     };
+    AddButton(mode);
     SwitchMode(mode);
-};
-mineralbutton.onclick = function() {
-    SwitchMode("mineral");
-};
-neonbutton.onclick = function() {
-    SwitchMode("neon");
-};
-ysbutton.onclick = function() {
-    SwitchMode("yellowsub");
-};
+});
 
-function SwitchMode(mode) {
+function SwitchMode(newmode) {
+    ChangeActive(newmode);
     RemoveSheets();
-    const modesheet = document.createElement("link");
-    modesheet.className = "modesheet";
-    modesheet.rel = "stylesheet";
-    modesheet.type = "text/css";
-    modesheet.href = `css/_${mode}.css`;
-    document.head.appendChild(modesheet);
-    if (mode != "lightmode") {
-        if (mode !="darkmode") {
-            AddButton(mode);
-        };
-    };
-};
+    AddSheets(newmode);
+    if (!isMysteryActive) {
+        isMysteryActive = true;
+        mysterybutton.style.display = "unset";
+    }
+}
 
-function AddButton(button) {
-    thisButton = document.getElementById(button);
-    thisButton.style.display = "unset";
-};
+function ChangeActive(newmode) {
+    document.getElementById(activemode).classList.remove("active");
+    activemode = newmode;
+    document.getElementById(activemode).classList.add("active");
+}
 
 function RemoveSheets() {
     for (let i=0; i < stylesheets.length; i++) {
         document.getElementsByClassName("modesheet")[i].remove();
     }
+}
+
+function AddSheets(newmode) {
+    const modesheet = document.createElement("link");
+    modesheet.className = "modesheet";
+    modesheet.rel = "stylesheet";
+    modesheet.type = "text/css";
+    modesheet.href = `css/_${newmode}.css`;
+    document.head.appendChild(modesheet);
+}
+
+function AddButton(addmode) {
+    const newButton = document.createElement("div");
+    newButton.className = "mode-button";
+    newButton.id = `${addmode}`;
+    modes.appendChild(newButton);
+    newButton.addEventListener("click", () => {SwitchMode(`${addmode}`);} );
 }
