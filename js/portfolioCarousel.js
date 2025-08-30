c08dfd3f459f6e0b440a9fb04db604ee40b3b3ca
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-08-30 14:09:21 +1000

    Added Portfolio carousel functionality, new styling, and media queries. Reduced the verbiage for each portfolio item. SFX: now the user can generate ripples by clicking over the text. Added desktop media query for hamburger menu icon. Stopped the GreenGeeks logo from being pushed upwards by its <a> element.

--- js/portfolioCarousel.js
+++ js/portfolioCarousel.js
@@ -3,60 +3,61 @@ const portfolioItems = [
         name: "CSSsandbox",
         articleLink: "/CSSsandbox.html",
         imgSrc: "img/folio_sandbox.png",
-        h3Text: "CSS/Javascript Design Showcase",
-        bodyText: "<p>Check out these neat designs and effects I've created. My favorite is one that looks iridescent.</p><p>Any of these designs and effects are available for use on your website. We can make adjustments to color palettes and other design elements to make something that works seamlessly with your brand.</p><p>Like my other sites, this design showcase is fully responsive for desktop and mobile devices.</p>",
+        h3Text: "Design Showcase",
+        bodyText: "<p>Check out these neat designs and effects I've made. My favorite is one that looks iridescent.</p>",
     },
     {
         name: "jewelery",
         href: "https://www.weatherheaddesigns.com",
         imgSrc: "img/folio_weatherheaddesigns.png",
         h3Text: "WeatherheadDesigns.com",
-        bodyText: "<p>My personal jewellery website. I'm an avid amateur jeweller in my spare time.</p><p>Features:</p><ul><li>Inventory can be updated quickly, easily and seamlessly: new product details get added to a database, and the code does the rest of the work.</li><li>Click on a thumbnail to see more information about each product.</li><li>The background is sparkly: try moving your mouse or tilting your mobile device.</li></ul>",
+        bodyText: "<p>My personal jewellery website. Inventory can be updated quickly, easily and seamlessly: new product details get added to a database, and the code does the rest of the work.</p>",
     },
     {
         name: "CreNav",
         href: "/CreNav.html",
         imgSrc: "img/folio_crenav.png",
         h3Text: "Implementing a more complex design",
-        bodyText: "<p>My replication of an existing website, with minor design updates for mobile/desktop optimization, readability, and harmonious color palette. The original website belongs to <a href='https:\\www.CreativeNavigation.com' target='_blank'>Creative Navigation</a>.</p><p>This was an exercise in designing a modern responsive site based on someone else's design specifications.</p><p>Features:</p><ul><li>Functional hamburger menu (on mobile, and on desktop when the browser window is narrow)</li><li>Functional 'Contact us' form</li><li>Interactive mouse-over effects on key images and navigation links</li></ul>",
+        bodyText: "<p>My replica of <a href='https://creativenavigation.com/'>an existing website</a>, with the addition of responsive design for mobile devices. It includes a functional \"Contact Us\" form and interactive mouse-over effects for key images and navigation links.</p>",
     },
     {
         name: "boognish",
         href: "http://www.whatdoesitween.com",
         imgSrc: "img/folio_boognish.png",
         h3Text: "WhatDoesItWeen.com",
-        bodyText: "<p>A fansite for the band Ween. It catalogs the wide range of musical genres and artists from whom they seem to have drawn influence for each of their songs.</p><p>Features:</p><ul><li>Updates are easy: the code automatically adds all listings from the database to the site.</li><li>Responsive design: Visitors see the same well-organized content on different window sizes, devices, and screen orientations.</li><li>Includes a fun snake game, custom-made from scratch.</li></ul>",
+        bodyText: "<p>A fansite for the band Ween. Updates are easy: the code automatically adds all song listings from the database. The site also features a snake game.</p>",
     },
     {
         name: "pianoLessons",
         href: "piano/index.html",
         imgSrc: "img/folio_pianolessons.png",
-        h3Text: "Piano teacher website for my friend",
-        bodyText: "<p>A website I made for my friend. It's no longer online. I've replicated it here; some of the wording has been replaced.</p><p>Features:</p><ul><li>Simple, classic design that aligns with the topic of the website and with the customer's vision.</li><li>Responsive design: Visitors see the same well-organized content on different window sizes, devices, and screen orientations.</li><li>Like other websites in this portfolio, this site features multiple pages and each page contain links to other pages on the site.</li></ul>",
+        h3Text: "Multi-page small-business site",
+        bodyText: "<p>A website I made for my friend. It's no longer online. I've replicated it here (some of the wording has been replaced for privacy reasons).</p>",
     },
     {
         name: "portfolio_v2",
         href: "v2/index.html",
         imgSrc: "img/folio_portfolio_v2.png",
-        h3Text: "Previous version of this website",
-        bodyText: "<p>This is the previous version of the website.</p><p>Features:</p><ul><li>Visitors can choose dark mode for lower eye strain and reduced battery use.</li><li>Responsive design: Visitors see the same well-organized content on different window sizes, devices, and screen orientations.</li></ul>",
+        h3Text: "This website (2024-2025)",
+        bodyText: "<p>Here's a previous version of this website. The front page features a shimmering-text effect. I had some fun with the light/dark mode settings.</p>",
     },
     {
         name: "this",
         href: "#Home",
         imgSrc: "img/folio_this.png",
         h3Text: "WeatherheadOnline.com",
-        bodyText: "<p>This website! It's pretty great.</p><p>Features:</p><ul><li>Visitors can choose dark mode for lower eye strain and reduced battery use.</li><li>Responsive design: Visitors see the same well-organized content on different window sizes, devices, and screen orientations.</li></ul>",
+        bodyText: "<p>This website! It's pretty great. Stop by the Special Effects section if you haven't already. Thanks for visiting!</p>",
     },
 ]
 
-const article = document.getElementById("folioArticle");
-const imgLink = article.children[0];
-const image = article.children[0].children[0];
-const articleText = document.getElementById("articleText");
-const articleBody = document.getElementById("articleBody");
-const h3Link = articleText.children[0];
-const h3Text = articleText.children[0].children[0];
+// const article = document.getElementById("portfolio-contents");
+const imgLink = document.getElementById("portfolio-img").children[0];
+const image = imgLink.children[0];
+const articleBody = document.getElementById("portfolio-text-body");
+const textContainer = document.getElementById("portfolio-item-text");
+const h3Link = textContainer.children[0];
+const h3Text = textContainer.children[0].children[0];
+
 const folioLeftArrow = document.getElementById("portfolio_leftArrow");
 const folioRightArrow = document.getElementById("portfolio_rightArrow");
 const portfolioThumbnails = document.getElementById("portfolio_thumbnails");
@@ -78,6 +79,7 @@ const folioButtons = Array.from(document.getElementsByClassName("folio_thumbnail
 folioButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
         populate(index);
+        currentArticle = index;
     });
 });
 
@@ -95,9 +97,6 @@ function handleKeydown(event) {
     }
 };
 
-
-
-
 function populate(currentArticle) {
     imgLink.href = portfolioItems[currentArticle].articleLink;
     image.src = portfolioItems[currentArticle].imgSrc;
@@ -107,7 +106,6 @@ function populate(currentArticle) {
 }
 
 function folioMoveLeft() {
-    removeSFXclasses(currentArticle);
     if(currentArticle > 0) {
         currentArticle--;
     } else {
@@ -117,7 +115,6 @@ function folioMoveLeft() {
 }
 
 function folioMoveRight() {
-    removeSFXclasses(currentArticle);
     if(currentArticle < portfolioItems.length - 1) {
         currentArticle++;
     } else {
--- js/splash-text-bkgd.js
+++ js/splash-text-bkgd.js
@@ -17,21 +17,21 @@ function addMobileListeners() {
     window.addEventListener("deviceorientation", function(event){    
         if (screen.height > screen.width) {  // ie in portrait
             if (event.gamma < -45) {
-                movingBkgd.style.left = `${self.innerWidth * 2}px`;
+                splash.style.left = `${self.innerWidth * 2}px`;
             } else if (event.gamma > 45) {
-                movingBkgd.style.left = "0";
+                splash.style.left = "0";
             } else {
-                movingBkgd.style.left = `${self.innerWidth * 2 / 90 * event.gamma - self.innerWidth}px`;  
+                splash.style.left = `${self.innerWidth * 2 / 90 * event.gamma - self.innerWidth}px`;  
                         // 2 refers to the 2 x self.innerWidth distance the background moves, between gamma=-45 and gamma=45.
                         // 90 refers to gamma degrees between -45 to the left and 45 to the right
             };
         } else {
             if (event.beta < -45) {
-                movingBkgd.style.left = `${self.innerWidth * 2}px`;
+                splash.style.left = `${self.innerWidth * 2}px`;
             } else if (event.beta > 45) {
-                movingBkgd.style.left = "0";
+                splash.style.left = "0";
             } else {
-                movingBkgd.style.left = `${self.innerWidth * 2 / 90 * event.beta - self.innerWidth}px`;  
+                splash.style.left = `${self.innerWidth * 2 / 90 * event.beta - self.innerWidth}px`;  
                         // 2 refers to the 2 x self.innerWidth distance the background moves, between beta=-45 and beta=45.
                         // 90 refers to beta degrees between -45 to the left and 45 to the right
             };
