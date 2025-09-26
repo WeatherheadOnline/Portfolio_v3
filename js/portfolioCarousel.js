f4a9b004e1e4016dcb8ed556677bea8c15a52b79
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-09-26 19:55:12 +1000

    Added an alt attribute for the main image and the thumbnails

--- js/portfolioCarousel.js
+++ js/portfolioCarousel.js
@@ -3,6 +3,7 @@ const portfolioItems = [
         name: "holographMenu",
         articleLink: "/input-menu/",
         imgSrc: "img/folio_holographMenu.png",
+        imgAlt: "the website for the new form input menu",
         thumbSrc: "img/folio_holographMenu_small.png",
         h3Text: 'Mobile accessibility "input tool"',
         bodyText: "<p>Form input fields can be difficult to reach when you're using your mobile device one-handed. I've created this innovative input menu to improve accessibility and bring form inputs within easy reach of your thumbs.</p>",
@@ -12,6 +13,7 @@ const portfolioItems = [
         name: "jewelery",
         articleLink: "https://www.weatherheaddesigns.com",
         imgSrc: "img/folio_weatherheaddesigns.png",
+        imgAlt: "the website Weatherhead Designs",
         thumbSrc: "img/folio_weatherheaddesigns_small.png",
         h3Text: "WeatherheadDesigns.com",
         bodyText: "<p>My personal jewellery website. The code allows inventory to be updated quickly, easily and seamlessly, just by adding new product details to the existing list.</p>",
@@ -20,6 +22,7 @@ const portfolioItems = [
         name: "CSSsandbox",
         articleLink: "/CSSsandbox/",
         imgSrc: "img/folio_sandbox.png",
+        imgAlt: "a website showcasing designs and effects by the author",     
         thumbSrc: "img/folio_sandbox_small.png",
         h3Text: "Design Showcase",
         bodyText: "<p>Check out these designs and effects I've made. My favorite is one that looks like an iridescent gemstone when the user moves their mouse or tilts their device.</p>",
@@ -28,6 +31,7 @@ const portfolioItems = [
         name: "pianoLessons",
         articleLink: "/piano/",
         imgSrc: "img/folio_pianolessons.png",
+        imgAlt: "a website made for a client to advertise piano lessons",
         thumbSrc: "img/folio_pianolessons_small.png",
         h3Text: "Multi-page site for a client",
         bodyText: "<p>A website I designed and made for a local client. It's no longer online; I've replicated it here (some of the wording has been replaced for privacy reasons).</p>",
@@ -36,6 +40,7 @@ const portfolioItems = [
         name: "CreNav",
         articleLink: "/CreNav",
         imgSrc: "img/folio_crenav.png",
+        imgAlt: "a website made to the specifications of Creative Navigation",
         thumbSrc: "img/folio_crenav_small.png",
         h3Text: "Developing from specifications",
         bodyText: "<p>My replica of <a href='https://creativenavigation.com/' target='_blank'>an existing website</a>, with the addition of responsive design for mobile devices. It includes a functional \"Contact Us\" form, plus interactive mouse-over effects for user engagement.</p>",
@@ -44,6 +49,7 @@ const portfolioItems = [
         name: "boognish",
         articleLink: "http://www.whatdoesitween.com",
         imgSrc: "img/folio_boognish.png",
+        imgAlt: "the website www.whatdoesitween.com",
         thumbSrc: "img/folio_boognish_small.png",
         h3Text: "WhatDoesItWeen.com",
         bodyText: "<p>An old fansite for the band Ween. Updates are easy: the code automatically looks through all song listings and adds them to the site. Just for fun, the site also features a themed snake game.</p>",
@@ -52,6 +58,7 @@ const portfolioItems = [
         name: "portfolio_v2",
         articleLink: "/v2/",
         imgSrc: "img/folio_portfolio_v2.png",
+        imgAlt: "the previous version of this website",
         thumbSrc: "img/folio_portfolio_v2_small.png",
         h3Text: "This website (2024-2025)",
         bodyText: "<p>Here's a previous version of this website. The front page features a shimmering-text effect. (Hint: take a look at the light/dark mode settings.)</p>",
@@ -60,6 +67,7 @@ const portfolioItems = [
         name: "this",
         articleLink: "#Home",
         imgSrc: "img/folio_this.png",
+        imgAlt: "the website you're at right now",
         thumbSrc: "img/folio_this_small.png",
         h3Text: "WeatherheadOnline.com",
         bodyText: "<p>This website! It's pretty great. Did you notice? - the text at the top of the page responds to moving your mouse or tilting your device. Stop by the Special Effects section if you haven't already. Thanks for visiting!</p>",
@@ -86,6 +94,7 @@ for (let article = 0; article <= portfolioItems.length - 1; article++) {
     buttonDiv.className = "folio_thumbnail";
     buttonDiv.id = portfolioItems[article].name;
     buttonDiv.src = portfolioItems[article].thumbSrc;
+    buttonDiv.alt = "A preview of " + portfolioItems[article].imgAlt;
     portfolioThumbnails.appendChild(buttonDiv);
 }
 
@@ -113,6 +122,7 @@ function handleKeydown(event) {
 function populate(currentArticle) {
     imgLink.href = portfolioItems[currentArticle].articleLink;
     image.src = portfolioItems[currentArticle].imgSrc;
+    image.alt = "A preview of " + portfolioItems[currentArticle].imgAlt;
     h3Link.href = portfolioItems[currentArticle].articleLink;
     h3Text.innerText = portfolioItems[currentArticle].h3Text;
     articleBody.innerHTML = portfolioItems[currentArticle].bodyText;
